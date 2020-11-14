256317fc49d8801ff62add580ae26883049bcabb
Linda <linda.andersson93@gmail.com>
2020-11-14 16:41:35 +1100

    added endpoint to all ads, changed targeted element to img so that card flips again

--- server.js
+++ server.js
@@ -6,23 +6,32 @@ const {adsWithState} = require("./adsWithState.js")
 app.use(express.static('public'))
 
 app.get('/api/ads/:state/:profile', (req, res) => {
-    var array = [];
-    var blackPower = ["Understanding racial segregation in the united states",
+    var adsInState = [];
+    const blackPower = ["Understanding racial segregation in the united states",
     "American Black Film Festival",
     "Cop Block",
     "HuffPost Black Voices", "Pan-Africanism", "Black Panther Party", "Black panther", "Black (Color)"];
+    const antiMuslim = [];
+    const whiteNationalist = [];
+    const Hispanic = [];
+    const lgbtq = [];
+    const musilm = [];
+    const prisoners = [];
+    const concervativeGunOwners = [];
+    const libertarians = [];
+    const policeForce = [];
 
     let data = Object.entries(adsWithState)
     for (let i = 0; i < data.length; i++){
         const isState = data[i][1].state
-        const profile = req.params.profile
         if (isState && isState === req.params.state) {
-            array.push(data[i])
+            adsInState.push(data[i])
         }
     }
-    // console.log(array[0][1])
+    const profile = req.params.profile
+
     var newArray = []
-    array.forEach(ad => {
+    adsInState.forEach(ad => {
         if(ad[1].interests != null){
             blackPower.forEach(interest => {
                 if(ad[1].interests.includes(interest)){
@@ -31,7 +40,18 @@ app.get('/api/ads/:state/:profile', (req, res) => {
                 }
 
             })
-        }})
+        }
+    })
+    adsInState.forEach(ad => {
+        if(ad[1].interests_also_match != null){
+            blackPower.forEach(interest => {
+                if(ad[1].interests_also_match.includes(interest)){
+                    if(!newArray.includes(ad)){
+                    newArray.push(ad)}
+                }
+            })
+        }
+    })
     res.send(newArray.flat())
 })
 
@@ -50,7 +70,14 @@ app.get('/api/ads/:state', (req, res) => {
 
 })
 
-
+app.get('/api/ads', (req, res) => {
+    var allAds = [];
+    let data = Object.entries(adsWithState)
+    for (let i = 0; i < data.length; i++){
+            allAds.push(data[i])
+    }
+    res.send(allAds.flat())
+})
 
 
 app.listen(4567, () => {
