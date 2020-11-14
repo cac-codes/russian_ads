15c43cdddbfda63e2cee39e4f00f6c36c63c3f6e
Linda <linda.andersson93@gmail.com>
2020-11-14 15:55:20 +1100

    new endpoint state/profile

--- server.js
+++ server.js
@@ -5,6 +5,36 @@ const axios = require('axios')
 const {adsWithState} = require("./adsWithState.js")
 app.use(express.static('public'))
 
+app.get('/api/ads/:state/:profile', (req, res) => {
+    var array = [];
+    var blackPower = ["Understanding racial segregation in the united states",
+    "American Black Film Festival",
+    "Cop Block",
+    "HuffPost Black Voices", "Pan-Africanism", "Black Panther Party", "Black panther", "Black (Color)"];
+
+    let data = Object.entries(adsWithState)
+    for (let i = 0; i < data.length; i++){
+        const isState = data[i][1].state
+        const profile = req.params.profile
+        if (isState && isState === req.params.state) {
+            array.push(data[i])
+        }
+    }
+    // console.log(array[0][1])
+    var newArray = []
+    array.forEach(ad => {
+        if(ad[1].interests != null){
+            blackPower.forEach(interest => {
+                if(ad[1].interests.includes(interest)){
+                    if(!newArray.includes(ad)){
+                    newArray.push(ad)}
+                }
+
+            })
+        }})
+    res.send(newArray.flat())
+})
+
 app.get('/api/ads/:state', (req, res) => {
     var array = []
 
@@ -19,6 +49,8 @@ app.get('/api/ads/:state', (req, res) => {
     res.send(array.flat())
 
 })
+
+
 
 
 app.listen(4567, () => {
