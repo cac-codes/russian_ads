42013f1c39436ec6d3aface056571278b37d0540
Linda <linda.andersson93@gmail.com>
2020-11-15 11:00:53 +1100

    target groups end point

--- server.js
+++ server.js
@@ -3,56 +3,68 @@ const app = express();
 const axios = require('axios')
 
 const {adsWithState} = require("./adsWithState.js")
+const targetGroups = require("./targetGroups.js")
 app.use(express.static('public'))
 
 app.get('/api/ads/:state/:profile', (req, res) => {
-    var adsInState = [];
-    const blackPower = ["Understanding racial segregation in the united states",
-    "American Black Film Festival",
-    "Cop Block",
-    "HuffPost Black Voices", "Pan-Africanism", "Black Panther Party", "Black panther", "Black (Color)"];
-    const antiMuslim = [];
-    const whiteNationalist = [];
-    const Hispanic = [];
-    const lgbtq = [];
-    const musilm = [];
-    const prisoners = [];
-    const concervativeGunOwners = [];
-    const libertarians = [];
-    const policeForce = [];
-
-    let data = Object.entries(adsWithState)
-    for (let i = 0; i < data.length; i++){
-        const isState = data[i][1].state
-        if (isState && isState === req.params.state) {
-            adsInState.push(data[i])
-        }
+    
+    let profile = '';
+    if(req.params.profile == "blackPower"){
+        profile =  targetGroups.blackPower
+    }else if(req.params.profile == "antiMuslim"){
+        profile = targetGroups.antiMuslim
+    }else if(req.params.profile == "whiteNationalist"){
+        profile = targetGroups.whiteNationalist
+    }else if(req.params.profile == "Hispanic"){
+        profile = targetGroups.Hispanic
+    }else if(req.params.profile == "lgbtq"){
+        profile = targetGroups.lgbtq
+    }else if(req.params.profile == "muslim"){
+        profile = targetGroups.muslim
+    }else if(req.params.profile == "prisoners"){
+        profile = targetGroups.prisoners
+    }else if(req.params.profile == "concervativeGunOwners"){
+        profile = targetGroups.concervativeGunOwners
+    }else if(req.params.profile == "libertarians"){
+        profile = targetGroups.libertarians
+    }else if(req.params.profile == "policeForce"){
+        profile = targetGroups.policeForce
     }
-    const profile = req.params.profile
 
-    var newArray = []
-    adsInState.forEach(ad => {
-        if(ad[1].interests != null){
-            blackPower.forEach(interest => {
-                if(ad[1].interests.includes(interest)){
-                    if(!newArray.includes(ad)){
-                    newArray.push(ad)}
-                }
+    function adsByProfile(targetGroup){
+        var adsInState = [];
+        let data = Object.entries(adsWithState)
+        for (let i = 0; i < data.length; i++){
+            const isState = data[i][1].state
+            if (isState && isState === req.params.state) {
+                adsInState.push(data[i])
+            }
+        }
+        var newArray = []
+        adsInState.forEach(ad => {
+            if(ad[1].interests != null){
+                targetGroup.forEach(interest => {
+                    if(ad[1].interests.includes(interest)){
+                        if(!newArray.includes(ad)){
+                        newArray.push(ad)}
+                    }
 
-            })
-        }
-    })
-    adsInState.forEach(ad => {
-        if(ad[1].interests_also_match != null){
-            blackPower.forEach(interest => {
-                if(ad[1].interests_also_match.includes(interest)){
-                    if(!newArray.includes(ad)){
-                    newArray.push(ad)}
-                }
-            })
-        }
-    })
-    res.send(newArray.flat())
+                })
+            }
+        })
+        adsInState.forEach(ad => {
+            if(ad[1].interests_also_match != null){
+                targetGroup.forEach(interest => {
+                    if(ad[1].interests_also_match.includes(interest)){
+                        if(!newArray.includes(ad)){
+                        newArray.push(ad)}
+                    }
+                })
+            }
+        })
+        return newArray
+    }
+    res.send(adsByProfile(profile).flat())
 })
 
 app.get('/api/ads/:state', (req, res) => {
