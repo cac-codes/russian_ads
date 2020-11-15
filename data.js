4e42b388eadfe8d0a231e9d5302fd3cac58c4841
Linda <linda.andersson93@gmail.com>
2020-11-15 12:32:50 +1100

    updated adsWithState to include all ads with state

--- data.js
+++ data.js
@@ -1,31 +1,16 @@
 var fs = require("fs")
 
-
-
-// fs.readFile("data.json", function (error, content){
-//     var stateData = JSON.parse(content)
-//     console.log(stateData.data[2].location_living[0].state)
-// })
-
-
-// var data = JSON.parse(fs.readFileSync(filePath));
-
-
 fs.readFile("data.json", function (error, content){
     
     var allAdsWithState = {}
 
     var stateData = JSON.parse(content)
-
-    // console.log(stateData.data[5].location_living == null)
-
-    // console.log(stateData.data[7])
    
     var counter = 0
 
     stateData.data.forEach( ad => {
         if(ad.location_living != null){
-            if(ad.location_living[0].state != undefined)
+            if(ad.location_living[0].state != undefined){
             // console.log(ad.location_living)
             counter++;
             var id = ad.ad_id
@@ -39,9 +24,25 @@ fs.readFile("data.json", function (error, content){
                 interests: ad.interests,
                 interests_also_match: ad.interests_also_match
             }
-        }
+            console.log(ad.location_living[0].state)
+            }
+        } else if(ad.location != null){
+            if(ad.location[0].state != undefined){
+            console.log(ad.location[0].state)
+            var id = ad.ad_id
+            allAdsWithState[id] = 
+            {
+                state: ad.location[0].state,
+                city: ad.location[0].city,
+                image: ad.cropped_image,
+                ad_spend: ad.ad_spend,
+                impressions: ad.impressions,
+                interests: ad.interests,
+                interests_also_match: ad.interests_also_match
+            }
+        }}
+
     });
     console.log(allAdsWithState)
-    fs.writeFileSync("adsWithState.js", JSON.stringify(allAdsWithState))
+    fs.writeFileSync("allAdsWithState.js", JSON.stringify(allAdsWithState))
 })
-
