bb95a5f013a1a7da57d24c805a5799c4c248966d
stuartmack <mackjohnstuart@gmail.com>
2020-11-16 12:22:13 +1100

    hover states displays state title; mouseleave map reverts to USA; dc circle fixed to behave like other states

--- public/javascript/svg-map.js
+++ public/javascript/svg-map.js
@@ -1,5 +1,35 @@
 // variable to select all states (path elements)
 let svgStates = document.querySelectorAll("#states > *");
+let dcCircle = document.querySelector("#DC");
+
+// entire map
+let svgMap = document.querySelector("#states");
+
+// state/country title below map
+let mapHeader = document.querySelector(".location-info-name");
+
+// target group table data fields
+let whitePrideData = document.querySelector(".white-pride-result");
+let blackPrideData = document.querySelector(".black-pride-result");
+let antiMuslimData = document.querySelector(".anti-muslim-result");
+let hispanicData = document.querySelector(".hispanic-result");
+let lgbtqData = document.querySelector(".lgbtq-result");
+let firstNationsData = document.querySelector(".first-nations-result");
+let farRightData = document.querySelector(".far-right-result");
+let muslimsData = document.querySelector(".muslims-result");
+let incarceratedData = document.querySelector(".incarcerated-result");
+let gunOwnersData = document.querySelector(".gun-owners-result");
+let libertariansData = document.querySelector(".libertarians-result");
+let policeData = document.querySelector(".police-result");
+let veteransData = document.querySelector(".veterans-result");
+let christiansData = document.querySelector(".christians-result");
+
+
+function adsByTarget(state) {
+    axios.get(`/api/ads/${state}/blackPower`).then(res => {
+        blackPrideData.textContent = res.data.length
+    })
+}
 
 // remove hover highlight as mouse leaves state, fade all states back in when mouse leaves 'clicked' state
 function hoverOff() {
@@ -12,6 +42,8 @@ function hoverOff() {
 // state highlighted on hover/mouseenter
 function hoverHighlight(el) {
     el.classList.add("on");
+
+    mapHeader.textContent = el.getAttribute("data-state")
 }
 
 // fade rest of states and highlight state when state is clicked
@@ -27,6 +59,9 @@ function fadeStates(el) {
         el.classList.remove("faded-state");
         el.classList.add("highlight-state");
     }
+    dcCircle.classList.add("faded-state");
+    let state = el.getAttribute("data-state");
+    adsByTarget(state);
 }
 
 // add event listeners to state elements
@@ -41,4 +76,9 @@ svgStates.forEach((el) => {
         fadeStates(el);
     })
 });
+
+
+svgMap.addEventListener("mouseleave", () => {
+    mapHeader.textContent = "USA"
+})
 
