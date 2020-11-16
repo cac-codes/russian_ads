beceaf73874f688bd52549a226e3948f21e7ca8c
Linda <linda.andersson93@gmail.com>
2020-11-16 12:58:34 +1100

    new file includein all ads across the country

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const axios = require('axios')
 
 const {adsWithState} = require("./adsWithState.js")
 const {allAdsWithState} = require("./allAdsWithState.js")
+const {allAdsUSA} = require("./allAdsUSA.js")
 const targetGroups = require("./targetGroups.js")
 app.use(express.static('public'))
 
-app.get('/api/:profile', (req, res) => {
+app.get('/api/ads/:state/:profile', (req, res) => {
     
     let profile = '';
     if(req.params.profile == "blackPower"){
@@ -16,8 +17,8 @@ app.get('/api/:profile', (req, res) => {
         profile = targetGroups.antiMuslim
     }else if(req.params.profile == "whiteNationalist"){
         profile = targetGroups.whiteNationalist
-    }else if(req.params.profile == "hispanic"){
-        profile = targetGroups.hispanic
+    }else if(req.params.profile == "Hispanic"){
+        profile = targetGroups.Hispanic
     }else if(req.params.profile == "lgbtq"){
         profile = targetGroups.lgbtq
     }else if(req.params.profile == "muslim"){
@@ -33,34 +34,37 @@ app.get('/api/:profile', (req, res) => {
     }
 
     function adsByProfile(targetGroup){
-        var ads = [];
+        var adsInState = [];
         let data = Object.entries(allAdsWithState)
         for (let i = 0; i < data.length; i++){
-                ads.push(data[i])
+            const isState = data[i][1].state
+            if (isState && isState === req.params.state) {
+                adsInState.push(data[i])
+            }
         }
-        var targetedGroupAds = []
-        ads.forEach(ad => {
+        var newArray = []
+        adsInState.forEach(ad => {
             if(ad[1].interests != null){
                 targetGroup.forEach(interest => {
                     if(ad[1].interests.includes(interest)){
-                        if(!targetedGroupAds.includes(ad)){
-                        targetedGroupAds.push(ad)}
+                        if(!newArray.includes(ad)){
+                        newArray.push(ad)}
                     }
 
                 })
             }
         })
-        ads.forEach(ad => {
+        adsInState.forEach(ad => {
             if(ad[1].interests_also_match != null){
                 targetGroup.forEach(interest => {
                     if(ad[1].interests_also_match.includes(interest)){
-                        if(!targetedGroupAds.includes(ad)){
-                        targetedGroupAds.push(ad)}
+                        if(!newArray.includes(ad)){
+                        newArray.push(ad)}
                     }
                 })
             }
         })
-        return targetedGroupAds
+        return newArray
     }
     res.send(adsByProfile(profile).flat())
 })
@@ -82,12 +86,10 @@ app.get('/api/ads/:state', (req, res) => {
 
 app.get('/api/ads', (req, res) => {
     var allAds = [];
-    let data = Object.entries(allAdsWithState)
-    let data2 = Object.entries(adsWithState)
+    let data = Object.entries(allAdsUSA)
     for (let i = 0; i < data.length; i++){
             allAds.push(data[i])
     }
-    console.log(data.length)
     console.log(data.length)
     res.send(allAds.flat())
 })
